aa953b3f497a87a05aa942b78b9b86f4503e5c5b
juandsw <juandavidgallegohernandez@gmail.com>
2024-10-27 20:36:32 -0500

    Se crea el Apigateway para ucobet

---
+++ src/test/java/co/edu/uco/ucobet/Apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.ucobet.Apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
